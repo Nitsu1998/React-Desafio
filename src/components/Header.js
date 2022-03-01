17442452fb715bfaafd5bc15c508611c49f3c6e3
Agustin Micheletti <agusmicheletti1234@gmail.com>
2022-02-28 21:17:45 -0300

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
 import NavBar from "./NavBar";
 import CartWidget from "./CartWidget.js";
+import logo from "../img/logo.png"
 
 export default function Header() {
   return (
     <>
       <header id="header">
-
-        <div id="tittle">
+        <div id="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div id="title">
           <h1>RICK & MORTY</h1>
         </div>
-
         <NavBar />
-
-        <div id="nav-sign">
+        <div id="sign">
           <button>SIGN IN</button>
           <button>SIGN UP</button>
           <CartWidget />
         </div>
-
       </header>
     </>
   );
