871b9f838f191a58a425e27b88729380703eac35
Agustin Micheletti <agusmicheletti1234@gmail.com>
2022-03-05 13:12:09 -0300

    css

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,9 @@
 export default function Item(item) {
   return (
     <>
-      <div id="item">
+      <div id="product">
         <img src={item.img} width="250" height="250" alt="" />
-        <div id="infoItem">
+        <div id="infoProduct">
           <h3>{item.name}</h3>
           <h5>Price: {item.price} ETH</h5>
           <button>More Info</button>
