f77c165bf877735992fbdbd567a3d3009d609681
Agustin <agusmicheletti1234@gmail.com>
2022-03-09 13:12:02 -0300

    destructuring

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,18 +1,11 @@
 import Item from "./Item";
 
-export default function ItemList({ items }) {
+export default function ItemList({items}) {
   return (
     <>
       <div id="productsContainer">
         {items.map((item) => (
-          <Item
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            stock={item.stock}
-            price={item.price}
-            img={item.img}
-          />
+          <Item item={item}/>
         ))}
       </div>
     </>
