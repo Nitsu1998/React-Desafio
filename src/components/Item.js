c01976e01fa66460f8e49c4f1f8d6e3001f2a9ad
Agustin <agusmicheletti1234@gmail.com>
2022-03-09 17:47:08 -0300

    price

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,7 +8,7 @@ export default function Item({item}) {
         <img src={image} width="250" height="250" alt="" />
         <div id="infoProduct">
           <h3>{name}</h3>
-          <h5>Price: {Math.random()}ETH</h5>
+          <h5>Price: {0.8}ETH</h5>
           <button>More Info</button>
         </div>
       </div>
